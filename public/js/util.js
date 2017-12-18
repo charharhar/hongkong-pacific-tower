8a0fa3b3ff0f7d9cbf28d28df034335d6cc6e3dc
Charles Lee <charleslee90@gmail.com>
2017-12-18 16:35:50 +0800

    Finishes about page mobile

--- public/js/util.js
+++ public/js/util.js
@@ -12,7 +12,7 @@ function scrollTop() {
  */
 export function slickHelper(target) {
   $(target).slick({
-    autoplay: true,
+    // autoplay: true,
     arrows: true,
     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
